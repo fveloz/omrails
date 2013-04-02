f8d7f85af4995d12d654a52d4cfbf05a724b55b3
Francisco Veloz <fveloz@me.com>
2013-04-02 11:19:47 -0400

    added pins Assocations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,9 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true #looks up rails validations
+
+  belongs_to :user
+
+  validates :user_id, presence: true
+
 end
